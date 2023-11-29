013886b887a8224707d15bf517a21bb82617f901
Matias Cordero <corderomatias2013@gmail.com>
2023-11-29 16:23:45 -0300

    dasd

--- ProyectoTracKids/settings.py
+++ ProyectoTracKids/settings.py
@@ -28,11 +28,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 DEBUG = 'RENDER' not in os.environ
 
 # https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.2.2']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 
 # Application definition
@@ -163,3 +164,4 @@ CLOUDINARY = {
     'api_key': '369268768791138',
     'api_secret': '6HucCaibPEhkVm-W3JREtd0eNSo',
 }
+
